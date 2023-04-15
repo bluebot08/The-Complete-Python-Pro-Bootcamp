e078056166ba5f8b0577f290ce01a83462354b03
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-15 17:04:41 +0900

    Finished assignment

--- Section 1 - Day 1/Printing-to-Console/main.py
+++ Section 1 - Day 1/Printing-to-Console/main.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
